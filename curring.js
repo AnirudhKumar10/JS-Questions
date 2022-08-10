9a7d04b6b81512ed46b92957e78c25c8cb96096c
AnirudhKumar10 <anirudh3104@gmail.com>
2022-08-10 12:55:57 +0530

    Update curring.js

--- curring.js
+++ curring.js
@@ -1,10 +1,10 @@
 function add(...args){
 	let sum1 = args.length!==0 ? args.reduce((a, b)=>a+b) : 0
-  return function(...args) {
-    if(args.length===0) return sum1;
-    let sum2 = args.length!==0 ? args.reduce((a, b)=>a+b) : 0
-    return add(sum2 + sum1)
-  }
+	return function(...args) {
+	    if(args.length===0) return sum1;
+	    let sum2 = args.length!==0 ? args.reduce((a, b)=>a+b) : 0
+	    return add(sum2 + sum1)
+	}
 }
 
 console.log(add(1, 4, 2)()) //7
