cb2f01c29de422073caebdf710f1e460cfe4d929
AnirudhKumar10 <anirudh3104@gmail.com>
2022-08-10 12:54:59 +0530

    Create curring.js

---
+++ curring.js
@@ -0,0 +1,13 @@
+function add(...args){
+	let sum1 = args.length!==0 ? args.reduce((a, b)=>a+b) : 0
+  return function(...args) {
+    if(args.length===0) return sum1;
+    let sum2 = args.length!==0 ? args.reduce((a, b)=>a+b) : 0
+    return add(sum2 + sum1)
+  }
+}
+
+console.log(add(1, 4, 2)()) //7
+console.log(add(1, 2)(1)(10)()) //14
+console.log(add(1)(2)(3)(4)()) // 10
+console.log(add(1, 2)(2, 4, 5 ,5 ,6)()) // 25
